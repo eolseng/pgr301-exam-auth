0ca96de9bbf0a3d7e2c8e067025bde59564ef52f
Erik Olseng <e.olseng@gmail.com>
2020-11-19 22:32:49 +0100

    Update kegs

--- src/main/kotlin/no/eolseng/pgr301examauth/beer/Keg.kt
+++ src/main/kotlin/no/eolseng/pgr301examauth/beer/Keg.kt
@@ -7,7 +7,6 @@ import javax.validation.constraints.Max
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
 
-
 interface KegRepository : JpaRepository<Keg, Int> {
     fun findAllByOwner(owner: User): Set<Keg>
 }
@@ -24,25 +23,30 @@ class Keg(
         @get:NotNull
         var owner: User? = null,
 
-        @get:Min(10)
-        @get:Max(600)
+        @get:Min(MIN_CAPACITY.toLong())
+        @get:Max(MAX_CAPACITY.toLong())
         var capacity: Int = 10,
 
         @get:Min(0)
-        @get:Max(600L)
+        @get:Max(MAX_CAPACITY.toLong())
         var currentVolume: Int = 0
 
-)
+) {
+    companion object{
+        const val MIN_CAPACITY: Int = 0
+        const val MAX_CAPACITY: Int = 600
+    }
+}
 
 data class KegDto(
         val id: Int? = null,
         val ownerName: String? = null,
-        @get:Min(10, message = "Keg size must be between 10 dl and 600 dl.")
-        @get:Max(600, message = "Keg size must be between 10 dl and 600 dl.")
+        @get:Min(Keg.MIN_CAPACITY.toLong(), message = "Keg size must be between ${Keg.MIN_CAPACITY} dl and ${Keg.MAX_CAPACITY} dl.")
+        @get:Max(Keg.MAX_CAPACITY.toLong(), message = "Keg size must be between ${Keg.MIN_CAPACITY} dl and ${Keg.MAX_CAPACITY} dl.")
         val capacity: Int? = null,
 
         @get:Min(0, message = "Volume cannot be negative")
-        @get:Max(600, message = "Volume cannot be bigger than max keg size (600 dl)")
+        @get:Max(Keg.MAX_CAPACITY.toLong(), message = "Volume cannot be bigger than max keg size (${Keg.MAX_CAPACITY} dl)")
         val currentVolume: Int = 0
 )
 
